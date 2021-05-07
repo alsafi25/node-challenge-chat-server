1ce72147a03577fdeff012f0b1d252713b3d6b19
alsafi25 <62480618+alsafi25@users.noreply.github.com>
2021-05-07 01:17:25 +0100

    server.js file updataed

---
+++ messages.js
@@ -0,0 +1,62 @@
+const messages = [ 
+  {
+        "id": 0,
+        "from": "Bart",
+        "text": "Welcome to CYF chat system!"
+    },
+    {
+        "id": 1,
+        "from": "Adam",
+        "text": "Welcome system!"
+    },
+    {
+        "id": 2,
+        "from": "Zara",
+        "text": "Welcome!"
+    },
+    {
+        "id": 3,
+        "from": "Hanna",
+        "text": "Welcome to chat system!"
+    },
+    {
+        "id": 4,
+        "from": "Aron",
+        "text": "Welcome to CYF !"
+    },
+    {
+      "id": 5,
+      "from": "Moh",
+      "text": "I am Moh!"
+  },
+  {
+    "id": 6,
+    "from": "Adrian",
+    "text": "Welcome to my chat room!"
+},
+{
+  "id": 7,
+  "from": "Barbra",
+  "text": "This is a cool system!"
+},
+{
+  "id": 8,
+  "from": "Sara",
+  "text": "Wonder world!"
+},
+{
+  "id": 9,
+  "from": "Brooks",
+  "text": "Yallla shoooot"
+},
+{
+  "id": 10,
+  "from": "Tom",
+  "text": "This is just me"
+},
+{
+  "id": 11,
+  "from": "Harry",
+  "text": "You know I'm a prince"
+},
+]
